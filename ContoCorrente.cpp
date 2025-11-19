983d8eb750d9c123bae7ec1c60b53a7025dfe49f
EmanueleArrigoni22 <ema05.arrigoni@gmail.com>
2025-11-19 22:57:20 +0100

    Modificata in maniera corposa la funziona registra movimento, che deve essere ultimata con un ultima modifica(ho lasciato un todo con la specifica)

--- ContoCorrente.cpp
+++ ContoCorrente.cpp
@@ -13,3 +13,18 @@ ContoCorrente::ContoCorrente(std::string nominativo):saldoCorrente(0),nominativo
     }
     file.close();
 }
+
+bool ContoCorrente::registraMovimento(const Movimento &m) {
+    bool successo = false;
+    if (m.getRecord() != "") {
+        operazioni.push_back(std::make_unique<std::string>(m.getRecord()));
+        std::ofstream file(path);
+        if (!file.is_open()) {
+            std::cerr << "Errore: impossibile creare il file!" << std::endl;
+        }
+        file << m.getRecord();
+        this->saldoCorrente += m.getImporto();//TODO capire come aggiungere o togliere dal saldo in base al tipo di movimento(entrata o uscita)
+        successo = true;
+    }
+    return successo;
+}
